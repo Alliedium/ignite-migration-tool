6b000db6e72d29aa11ea3746879254bbd16d1d52
ebaranetsky <reader3dx@gmail.com>
2022-01-27 15:00:10 +0200

    improves types resolution for nested objects using avro props

--- products/core/src/main/java/org/alliedium/ignite/migration/util/UniqueKey.java
+++ products/core/src/main/java/org/alliedium/ignite/migration/util/UniqueKey.java
@@ -1,27 +1,10 @@
 package org.alliedium.ignite.migration.util;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public class UniqueKey {
 
-    private static final String recordTypeIdentifier = "_recordType_";
-
     public static String generate() {
         return "u_" + UUID.randomUUID().toString().replace("-", "_");
-    }
-
-    public static String generateWithRecordType(String type) {
-       return  UniqueKey.generate() + recordTypeIdentifier + type;
-    }
-
-    public static Optional<String> getRecordType(String uniqueKey) {
-        int index = uniqueKey.indexOf(recordTypeIdentifier);
-        if (index == -1) {
-            return Optional.empty();
-        }
-
-        String recordType = uniqueKey.substring(index + recordTypeIdentifier.length());
-        return Optional.of(recordType);
     }
 }
